ba359f1af4dd90e33f27a0b7fc3bba11d3e7bf55
Hisham <88942633+acse-hst21@users.noreply.github.com>
2023-12-25 18:16:04 +0000

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,11 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
         if x < 0:
-            is_negative = True 
-        else:
-            is_negative = False
-
-        if is_negative:
             reversed_int = int(str(x)[1:][::-1]) * -1
         else:
             reversed_int = int(str(x)[::-1])
