0304840268694a00af791e44fa4b29b274f7991b
Hisham <88942633+acse-hst21@users.noreply.github.com>
2024-02-11 17:39:28 +0000

    Create 287.py

---
+++ 287.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        my_set = set()
+        for num in nums:
+            if num in my_set:
+                return num
+            my_set.add(num)
