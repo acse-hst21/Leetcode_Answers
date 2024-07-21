93dcb7118d687fb87ea4c32e63ab282b5c2f2e5c
Hisham <88942633+acse-hst21@users.noreply.github.com>
2024-07-21 18:14:02 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        ans = []
-
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j:
-                    pass
-                else:
-                    if nums[i] + nums[j] == target:
-                        ans.append(i)
-                        ans.append(j)
-                        return ans
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+        for index, num in enumerate(nums):
+            if (target-num) in my_dict:
+                return [index, my_dict[target-num]]
+            my_dict[num] = index
