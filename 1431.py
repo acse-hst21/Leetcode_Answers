cf3d85e3771ab08a5f2c1e61de10990a220a1520
Hisham <88942633+acse-hst21@users.noreply.github.com>
2025-04-20 22:19:55 +0100

    Create 1431.py

---
+++ 1431.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_value = max(candies)
+        return [candy + extraCandies >= max_value for candy in candies]
